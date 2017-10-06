bd9c905d40ba6cb90ba71f11760f34163c82cf0b
yin qi wei <yinqiwei@yindeMacBook-Pro.local>
2017-10-06 14:50:21 +0800

    add

---
+++ httpserver.py
@@ -0,0 +1,13 @@
+try:
+    #for python3
+    from http.server import HTTPServer, CGIHTTPRequestHandler
+except ImportError:
+    #for python2
+    from BaseHTTPServer import HTTPServer
+    from CGIHTTPServer import CGIHTTPRequestHandler
+
+port = 8080
+
+httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
+print("Starting simple_httpd on port: " + str(httpd.server_port))
+httpd.serve_forever()
