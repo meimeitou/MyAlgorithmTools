f4fbb2e09bd5ea45b5eb779555173cd2d5592afa
yin qi wei <yinqiwei@yindeMacBook-Pro.local>
2017-10-06 11:19:45 +0800

    delete files

--- t7.py
+++
@@ -1,8 +0,0 @@
-n,m=[x for x in map(int,input().split())]
-
-minNu=n
-maxNu=6*n
-
-middle=(minNu+maxNu)//2
-
-
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'yinqiwei'
-
-
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
