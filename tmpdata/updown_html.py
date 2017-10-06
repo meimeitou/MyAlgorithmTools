b5ae1011d313bf68a51fc9734cca29f995632581
yin qi wei <yinqiwei@yindeMacBook-Pro.local>
2017-10-06 11:28:27 +0800

    or

--- tmpdata/updown_html.py
+++ tmpdata/updown_html.py
@@ -2,10 +2,10 @@ import urllib.request
 __author__ = 'yinqiwei'
 
 
-req=urllib.request.Request("http://www.baidu.com")
-html=urllib.request.urlopen(req)
+req = urllib.request.Request("http://www.baidu.com")
+html = urllib.request.urlopen(req)
 
-cont=html.readline()
+cont = html.readline()
 
 print(cont)
 print(html.info())
